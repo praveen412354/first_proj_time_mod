1d4d7aa9dc66b68d4886bd53fc08d30c786498a6
mirafra <burugupraveen90@gmail.com>
2026-07-06 11:54:30 +0530

    strftime function added

--- time_module.py
+++ time_module.py
@@ -2,3 +2,11 @@ import time
 
 print(time.time())
 print(time.ctime())
+current_date_time=time.localtime()
+print(type(current_date_time))
+print(current_date_time)
+
+print(time.strftime('%Y-%m-%d %H:%M:%S'))
+
+
+
